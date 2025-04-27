df87aee2b0e8a91646d1938d3fe4d14bb87dac93
matter143 <mnorris298@gmail.com>
2025-04-27 15:03:56 -0400

    Added rotation of player and resolved issues in main game loop

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
